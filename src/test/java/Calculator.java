714edc3403c37829da1e849dca26f4e633162ce1
nkosi <nkosi.cele@simplify.africa>
2025-02-12 19:34:25 +0200

    initial commit

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,44 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Calculator {
+    private AndroidDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("app", System.getProperty("user.dir") + "/src/main/Apps/Calculator.apk");
+//        caps.setCapability("noReset",true);
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), caps);
+    }
+
+    @Test
+    public void testAddition() {
+        // Example test case for addition
+
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_add")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+
+        assert driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText().equals("5");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
