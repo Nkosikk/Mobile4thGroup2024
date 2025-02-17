c418b81040d3ebf1f971a0b9b8601b9d2b470ad6
nkosi <nkosi.cele@simplify.africa>
2025-02-17 18:34:51 +0200

    created the runner class and added new applicable dependencies

--- src/test/java/Runners/runner.java
+++ src/test/java/Runners/runner.java
@@ -1,4 +1,14 @@
 package Runners;
 
-public class runner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/Features",
+        glue = {"Steps"},
+        tags = "",
+        plugin = {"pretty", "html:Reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        publish = true)
+public class runner extends AbstractTestNGCucumberTests {
 }
